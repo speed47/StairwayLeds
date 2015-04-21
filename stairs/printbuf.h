f0dd919c2f6d06e531684c06f3886b08acffcc62
speed47 <speed47_github@speed47.net>
2015-04-21 20:41:12 +0200

    modify printbuf to get -Wformat functionality

--- stairs/printbuf.h
+++ stairs/printbuf.h
@@ -5,35 +5,57 @@
 
 #define PRINT_BUFFER_SIZE 128
 
-#define DEBUG_LEVEL_1 1
-#define DEBUG_LEVEL_2 2
-#define DEBUG_LEVEL_3 3
+#define DEBUG_NONE    0
+#define DEBUG_MIN     1
+#define DEBUG_NORMAL  2
+#define DEBUG_FULL    3
 
 // should be defined via ./configure, but if not, here is a default
-#ifndef STAIRS_DEBUG
-# define STAIRS_DEBUG DEBUG_LEVEL_2
+#ifndef PROJECT_DEBUG
+# define PROJECT_DEBUG DEBUG_NORMAL
 #endif
 
-#if STAIRS_DEBUG >= DEBUG_LEVEL_1
-# define dbg1(...) dbg(1, __VA_ARGS__)
-#else
-# define dbg1(...) ;
-#endif
+char *printbuf(const char *format, ...)
+	__attribute__ ((format (printf, 1, 2)));
 
-#if STAIRS_DEBUG >= DEBUG_LEVEL_2
-# define dbg2(...) dbg(2, __VA_ARGS__)
-#else
-# define dbg2(...) ;
-#endif
-
-#if STAIRS_DEBUG >= DEBUG_LEVEL_3
-# define dbg3(...) dbg(3, __VA_ARGS__)
-#else
-# define dbg3(...) ;
-#endif
-
-char *printbuf(const char *format, ...);
 char *printbufva(const char *format, va_list args);
-void dbg(int level, const char *format, ...);
+
+void dbg(int level, const char *format, ...)
+	__attribute__ ((format (printf, 2, 3)));
+
+
+inline void dbg1(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+inline void dbg1(const char *format, ...)
+{
+#if PROJECT_DEBUG >= DEBUG_MIN
+	va_list args;
+	va_start(args,format);
+	dbg(1, format, args);
+	va_end(args);
+#endif
+}
+
+inline void dbg2(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+inline void dbg2(const char *format, ...)
+{
+#if PROJECT_DEBUG >= DEBUG_NORMAL
+	va_list args;
+	va_start(args,format);
+	dbg(2, format, args);
+	va_end(args);
+#endif
+}
+
+inline void dbg3(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+inline void dbg3(const char *format, ...)
+{
+#if PROJECT_DEBUG >= DEBUG_FULL
+	va_list args;
+	va_start(args,format);
+	dbg(3, format, args);
+	va_end(args);
+#endif
+}
+
 
 #endif
