6ff3f3dc563273ba5eb6251ac0d1da3b14b390ef
speed47 <speed47_github@speed47.net>
2015-04-22 22:27:43 +0200

    fix fake hue values in makeColor() doc and defines

--- stairs/makeColor.h
+++ stairs/makeColor.h
@@ -1,12 +1,18 @@
 #ifndef _MAKECOLOR_H
 #define _MAKECOLOR_H
 
-#define HUE_RED     0
-#define HUE_ORANGE  60
-#define HUE_YELLOW  120
-#define HUE_GREEN   180
-#define HUE_BLUE    240
-#define HUE_VIOLET  300
+#define HUE_RED          0
+#define HUE_ORANGE      30
+#define HUE_YELLOW      60
+#define HUE_LIGHTGREEN  90
+#define HUE_GREEN      120
+#define HUE_LIGHTCYAN  150
+#define HUE_CYAN       180
+#define HUE_LIGHTBLUE  210
+#define HUE_BLUE       240
+#define HUE_PINK       270
+#define HUE_VIOLET     300
+#define HUE_MAGENTA    330
 
 int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness);
 unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue);
