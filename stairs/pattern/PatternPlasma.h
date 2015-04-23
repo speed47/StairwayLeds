08dafb811c172bd04f660005a08bdbcb367800e5
speed47 <speed47_github@speed47.net>
2015-04-23 20:47:46 +0200

    better plasma maybe (blind)

--- stairs/pattern/PatternPlasma.h
+++ stairs/pattern/PatternPlasma.h
@@ -7,11 +7,12 @@
 class PatternPlasma : public Pattern
 {
 public:
-    PatternPlasma();
+    PatternPlasma(unsigned int duration = 5000);
 
     virtual inline const char* name() { return "Plasma"; }
 
 protected:
+    unsigned int _duration;
 
     virtual void _animate();
 };
